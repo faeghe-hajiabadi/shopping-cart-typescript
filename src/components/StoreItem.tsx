15e31360e544ac2a34fbb566d44193f4f447ffb1
Anna Abadi <faeghe.hajiabadi@gmail.com>
2024-11-05 16:56:45 -0800

    first commit

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,26 @@
+import { Card } from "react-bootstrap";
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  return (
+    <Card>
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      ></Card.Img>
+      <Card.Body className="d-lex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+            <span className="fs-2">{name}</span>
+            <span className="ms-2 text-muted">{price}</span>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  );
+}
